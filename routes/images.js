937b92e23201c42435ca59059e1efd7127b42f1d
刘燕保 <liuyanbao@shuidihuzhu.com>
2020-03-16 21:03:43 +0800

    feat: 测试middleware与asyncData完毕

--- routes/images.js
+++ routes/images.js
@@ -1,26 +1,24 @@
 /*
  * @Author: your name
- * @Date: 2020-03-16 19:47:42
- * @LastEditTime: 2020-03-16 21:03:14
+ * @Date: 2020-03-16 20:27:33
+ * @LastEditTime: 2020-03-16 20:28:25
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
- * @FilePath: /miniNodeSer/miniService/routes/users.js
+ * @FilePath: /miniNodeSer/miniService/routes/images.js
  */
 const router = require('koa-router')()
 
-router.prefix('/users')
+router.prefix('/images')
 
 router.get('/', function (ctx, next) {
   ctx.body = 'this is a users response!'
 })
 
-router.get('/userInfo', async (ctx, next) => {
+router.get('/getlogo', function (ctx, next) {
   ctx.body = {
     msg: '',
     data: {
-      name: 'liuyanbao',
-      age: 18,
-      speciality: 'long'
+      img: 'this is logo'
     }
   }
 })
